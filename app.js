d286f8138a0aba7bbc7499e20e6b5ce05b398912
SarayutBz <sarayut.aiamaurai@gmail.com>
2024-12-22 15:51:12 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+total = (a,b) =>{
+    return a+b
+}
+
+ console.log(total(10,20))
+ console.log(total("10",20))
+ console.log(total("10","20"))
+ console.log(total(true,false))
+ console.log(total(true,true))
---
+++ index.ts
@@ -0,0 +1,5 @@
+function formatNuber(num:number){
+    return num.toFixed(2)
+}
+let money:unknown = 50.123
+console.log(formatNuber(money))
