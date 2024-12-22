467c1d6f77b2bf4a930a7c9873139641518281be
SarayutBz <sarayut.aiamaurai@gmail.com>
2024-12-22 17:21:54 +0700

    เรียนจบแล้ว

--- dist/app.js
+++ dist/app.js
@@ -77,3 +77,82 @@ emp3.salary = 1;
 emp3.id = 0;
 console.log(emp3);
 // Array
+const user1 = "บอล";
+const user2 = "ปริม";
+const users = [];
+users.push("บอล");
+users.push("ปริม");
+console.log(users[1]);
+for (let i = 0; i < users.length; i++) {
+    console.log(`ลำดับที่ ${i + 1} = ${users[i]}`);
+}
+users.forEach(element => {
+    console.log(element);
+});
+const employees = [];
+employees.push({ name: "บอล", salary: 99999, department: "programmer" });
+employees.push({ name: "ปริม", salary: 22299999 });
+console.log(employees);
+function sayHii(name) {
+    if (!name) {
+        return 'Hello TypeScript';
+    }
+    if (typeof name == 'string') {
+        return `Hello ${name}`;
+    }
+    throw new Error("ชนิดข้อมุลไม่ถูกต้อง");
+}
+console.log(sayHii("sarayut"));
+function total(a, b) {
+    if (typeof a == "number" && typeof b == "number") {
+        return a + b;
+    }
+    if (typeof a == "string" && typeof b == "string") {
+        return parseInt(a) + parseInt(b);
+    }
+    throw new Error("ชนิดไม่ถูกต้อง");
+}
+console.log(total(100, 200));
+console.log(total("100", "200"));
+// console.log(total(true,"200"))
+// Spread Operator
+const section = ["บัญชี", "การเงิน"];
+const department2 = ["programmer", "travel", "ceo"];
+department2.push(...section);
+console.log(department2);
+// Rest Parameter
+const total2 = (...number) => {
+    return number.reduce((result, value) => {
+        return result + value;
+    }, 0);
+};
+console.log(total2(100, 200));
+console.log(total2(100, 200, 300));
+console.log(total2(100, 200, 300, 400));
+console.log(total2(100, 200, 300, 400, 500));
+// Destructuring (การสลายโครงสร้าง)
+const colors = ["แดง", "เขียว", "น้ำเงิน"];
+const [red, green, blue] = colors;
+console.log(red);
+console.log(green);
+console.log(blue);
+const person3 = {
+    fname: 'ก้อง',
+    age: 21,
+    address: "เชียงใหม่"
+};
+const { fname, age, address } = person3;
+console.log(fname);
+console.log(age);
+console.log(address);
+// Tuple
+const point = [10, 20];
+console.log(point);
+const goodStatus = [200, "ok"];
+console.log(goodStatus);
+// Tuple Label
+const point2 = [10, 20];
+const good = [200, 'ok'];
+const notFound = [404, "ไม่พบ"];
+const goodStatus2 = [200, "ok"];
+console.log(goodStatus2);
--- src/app.ts
+++ src/app.ts
@@ -123,4 +123,115 @@ console.log(emp3)
 
 
 // Array
+const user1:String = "บอล"
+const user2:String = "ปริม"
 
+const users:string[]=[]
+users.push("บอล")
+users.push("ปริม")
+
+console.log(users[1])
+
+for(let i=0;i<users.length;i++){
+    console.log(`ลำดับที่ ${i+1} = ${users[i]}`)
+}
+
+users.forEach(element=>{
+    console.log(element)
+})
+
+// Type Aliases Array
+type Employee4 ={
+    name:string,
+    salary:number,
+    department?:string
+}
+const employees:Employee4[] =[]
+employees.push({name:"บอล",salary:99999,department:"programmer"})
+employees.push({name:"ปริม",salary:22299999})
+console.log(employees)
+
+// Function Overloading
+function sayHii():string
+function sayHii(name:string):string
+function sayHii(name?:unknown):unknown{
+    if(!name){
+        return 'Hello TypeScript'
+    }
+    if(typeof name =='string'){
+        return `Hello ${name}`
+    }
+    throw new Error("ชนิดข้อมุลไม่ถูกต้อง")
+}
+console.log(sayHii("sarayut"))
+
+
+function total(a:number,b:number):number
+function total(a:string,b:string):string
+function total(a:unknown,b:unknown):unknown{
+    if(typeof a =="number" && typeof b== "number"){
+        return a+b
+    }
+    if(typeof a =="string" && typeof b== "string"){
+       return parseInt(a)+parseInt(b)
+    }
+    throw new Error("ชนิดไม่ถูกต้อง")
+}
+console.log(total(100,200))
+console.log(total("100","200"))
+// console.log(total(true,"200"))
+
+// Spread Operator
+const section = ["บัญชี","การเงิน"]
+const department2 = ["programmer","travel","ceo"]
+department2.push(...section)
+
+console.log(department2)
+
+// Rest Parameter
+const total2 = (...number:number[])=>{
+    return number.reduce((result,value)=>{
+        return result+value
+    },0)
+}
+console.log(total2(100,200))
+console.log(total2(100,200,300))
+console.log(total2(100,200,300,400))
+console.log(total2(100,200,300,400,500))
+
+// Destructuring (การสลายโครงสร้าง)
+const colors:string[] =["แดง","เขียว","น้ำเงิน"]
+const [red,green,blue] =  colors
+
+console.log(red)
+console.log(green)
+console.log(blue)
+
+const person3={
+    fname:'ก้อง',
+    age:21,
+    address:"เชียงใหม่"
+}
+
+const {fname,age,address} = person3
+console.log(fname)
+console.log(age)
+console.log(address)
+
+// Tuple
+const point:[number,number] = [10,20]
+console.log(point)
+const goodStatus:[number,string] =[200,"ok"]
+console.log(goodStatus)
+
+// Tuple Label
+const point2:[x:number,y:number] = [10,20]
+const good:[code:number,state:string] =[200,'ok']
+
+// type แบบ tuple
+type HttpStatusCode=[number,string]
+
+const notFound:HttpStatusCode = [404,"ไม่พบ"]
+const goodStatus2:HttpStatusCode =[200,"ok"]
+
+console.log(goodStatus2)
